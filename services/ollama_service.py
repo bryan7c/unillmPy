778d9ecbd58556c00624ce54a4d91d37f9122bb0
Bryan Marvila <bryan7c@gmail.com>
2024-11-11 18:17:44 -0300

    Add Ollama

--- services/ollama_service.py
+++ services/ollama_service.py
@@ -1,15 +1,11 @@
-import requests
-from config import Config
+from langchain_community.llms import Ollama
 
 class OllamaService:
+    def __init__(self):
+        # Inicializa o modelo Ollama com as configurações necessárias
+        self.model = Ollama(model="mistral-nemo:latest")
+
     def generate_text(self, input_text: str, options: dict = None) -> str:
-        headers = {
-            'Content-Type': 'application/json'
-        }
-        data = {
-            'model': options.get('model', 'mistral-nemo:latest'),
-            'messages': [{'role': 'user', 'content': input_text}]
-        }
-        response = requests.post(f"{Config.OLLAMA_BASE_URL}/generate", json=data, headers=headers)
-        response.raise_for_status()
-        return response.json()  # Ajuste conforme a estrutura da resposta
+        # Gera texto usando o modelo Ollama
+        llm = Ollama(model="llava:13b")
+        return llm.invoke(input_text)
